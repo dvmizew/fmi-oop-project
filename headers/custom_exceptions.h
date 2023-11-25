a58b93024de66d15682c53d04b3908bad19adf70
dvmizew <89708521+dvmizew@users.noreply.github.com>
2023-11-26 00:51:52 +0200

    added custom exceptions library

--- headers/custom_exceptions.h
+++ headers/custom_exceptions.h
@@ -5,4 +5,20 @@
 #ifndef OOP_CUSTOM_EXCEPTIONS_H
 #define OOP_CUSTOM_EXCEPTIONS_H
 
+#include <exception>
+
+class InvalidIndexException : public std::exception {
+public:
+    const char *what() const noexcept override {
+        return "Invalid index!\n";
+    }
+};
+
+class NullPointerException : public std::exception {
+public:
+    const char *what() const noexcept override {
+        return "Null pointer!\n";
+    }
+};
+
 #endif //OOP_CUSTOM_EXCEPTIONS_H
