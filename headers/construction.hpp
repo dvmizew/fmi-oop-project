e97e59d89ebef9aac02956b4c261f9718caf02fb
dvmizew <89708521+dvmizew@users.noreply.github.com>
2023-11-25 21:35:47 +0200

    added spellFactory and fixed the constructors

--- headers/construction.hpp
+++ headers/construction.hpp
@@ -6,7 +6,9 @@
 
 #include <vector>
 #include <queue>
+#include <memory>
 #include "troop.hpp"
+#include "spell.hpp"
 
 class construction {
 private:
@@ -27,21 +29,37 @@ public:
 std::ostream &operator<<(std::ostream &out, const construction &obj);
 
 // class for the barracks construction
-class barracks : construction {
+class barracks : public construction {
 private:
     unsigned int trainingRate{};
-    std::vector<troop> availableTroops{};
-    std::queue<troop> troopsInQueue{};
+    std::vector<std::unique_ptr<troop>> availableTroops{};
+    std::queue<std::unique_ptr<troop>> troopsInQueue{};
 public:
-    barracks(unsigned int hp, unsigned int trainingRate, std::vector<troop> availableTroops,
-             std::queue<troop> troopsInQueue)
-            : construction(hp), trainingRate(trainingRate), availableTroops(std::move(availableTroops)),
-              troopsInQueue(std::move(troopsInQueue)) {}
+    barracks(unsigned int hp, unsigned int trainingRate,
+             std::vector<std::unique_ptr<troop>> &&availableTroops,
+             std::queue<std::unique_ptr<troop>> &&troopsInQueue)
+            : construction(hp), trainingRate(trainingRate),
+              availableTroops(std::move(availableTroops)), troopsInQueue(std::move(troopsInQueue)) {}
 
     ~barracks() = default;
 
     // friend function for overloading <<
     friend std::ostream &operator<<(std::ostream &, const barracks &);
+};
+
+class spellFactory : public construction {
+private:
+    unsigned int brewingRate{};
+    std::vector<std::unique_ptr<spell>> availableSpells{};
+    std::queue<std::unique_ptr<spell>> spellsInQueue{};
+public:
+    spellFactory(unsigned int hp, unsigned int brewingRate,
+                 std::vector<std::unique_ptr<spell>> &&availableSpells,
+                 std::queue<std::unique_ptr<spell>> &&spellsInQueue)
+            : construction(hp), brewingRate(brewingRate),
+              availableSpells(std::move(availableSpells)), spellsInQueue(std::move(spellsInQueue)) {}
+
+    ~spellFactory() = default;
 };
 
 // prototype of the overloaded operator<< for barracks class
